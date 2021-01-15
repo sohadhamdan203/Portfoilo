4cd6362e1d4b97a5d9ff72e0e465a436496df8f6
DESKTOP-UECHB2U\HP <hajjajsohad@gmail.com>
2021-01-15 21:25:20 +0200

    Add project files.

---
+++ Core/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public class Category : EntityBase
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+
+    }
+
+}
